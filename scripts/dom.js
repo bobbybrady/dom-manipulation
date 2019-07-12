b3619fbfe15731a627fb8d8bb5003ad46e9b97eb
Bobby Brady <bobbybrady@Bobbys-MacBook-Pro-2.local>
2019-07-12 15:04:51 -0500

    did more exercises

--- scripts/dom.js
+++ scripts/dom.js
@@ -33,3 +33,67 @@ border.classList.add('border')
 sectionEls.forEach((currentNode) => {
     currentNode.classList.add('background')
 })
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+
+// console.log(wardrobe.height)
+// console.log(wardrobe.manufacturer)
+// console.log(wardrobe.contents)
+// console.log(wardrobe.depth)
+// console.log(wardrobe.width)
+
+const empireStateBuilding = {
+    stories: 103,
+    height: 1453,
+    address: "350 Fifth Avenue, Manhattan, New York 10118",
+    squareFeet: 2768591,
+    constructionDate: 1931,
+    cost: 40948900,
+    owner: "Empire State Realty Trust",
+    eastWestLength: 424,
+    northSouthLength: 187,
+    architect: "Shreve, Lamb & Harmon"
+}
+// console.log(empireStateBuilding.stories)
+// console.log(empireStateBuilding.height)
+// console.log(empireStateBuilding.squareFeet)
+// console.log(empireStateBuilding.eastWestLength)
+// console.log(empireStateBuilding.northSouthLength)
+
+const address = empireStateBuilding["address"]
+const squareFeet = empireStateBuilding["squareFeet"]
+const cost = empireStateBuilding["cost"]
+const owner = empireStateBuilding["owner"]
+const architect = empireStateBuilding["architect"]
+
+// console.log(address)
+// console.log(squareFeet)
+// console.log(cost)
+// console.log(owner)
+// console.log(architect)
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+
+const partTime = nashvilleSoftwareSchool.instructors.partTime
+partTime.forEach((currentNode) => {
+    console.log(currentNode)
+})
+const fullTime = nashvilleSoftwareSchool.instructors.fullTime
+fullTime.forEach((currentNode) => {
+    console.log(currentNode)
+})
+ console.log(nashvilleSoftwareSchool.instructors.fullTime[4])
+ console.log(nashvilleSoftwareSchool.instructors.partTime[0])
